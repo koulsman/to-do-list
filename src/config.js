c6ac84ee81bae12ac5a0da285af3f0fd2bde2b99
Stavros Koulas <koulas@verusplus.com>
2025-07-26 14:55:24 +0300

    fix in config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
+const trimSlash = (url) => url.replace(/\/+$/, '');
+
 const isProduction = process.env.NODE_ENV === "production";
+
+const baseProdUrl = trimSlash(process.env.REACT_APP_API_BASE_PROD);
 
 const config = {
   USERS_API: isProduction
-    ? `${process.env.REACT_APP_API_BASE_PROD}/UserServer`
+    ? `${baseProdUrl}/UserServer`
     : process.env.REACT_APP_USERS_API_DEV,
 
   LISTS_API: isProduction
-    ? `${process.env.REACT_APP_API_BASE_PROD}/ListServer`
+    ? `${baseProdUrl}/ListServer`
     : process.env.REACT_APP_LISTS_API_DEV,
-
-
 };
 
 export default config;
